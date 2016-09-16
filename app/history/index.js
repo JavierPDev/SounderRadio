8374a6f1c0610e087ba6f26509b08e1b1d8e9a24
Javier Pena <javier@javierdev.com>
2016-09-16 12:35:00 -0500

    Deploy with new dark ui and history module

---
+++ app/history/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./history.component'));
+__export(require('./history.routes'));
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9oaXN0b3J5L2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxpQkFBYyxxQkFBcUIsQ0FBQyxFQUFBO0FBQ3BDLGlCQUFjLGtCQUFrQixDQUFDLEVBQUEiLCJmaWxlIjoiYXBwL2hpc3RvcnkvaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL2hpc3RvcnkuY29tcG9uZW50JztcbmV4cG9ydCAqIGZyb20gJy4vaGlzdG9yeS5yb3V0ZXMnO1xuIl19
