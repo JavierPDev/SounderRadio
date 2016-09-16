7dffa19556a50c34a968edc733de24fae1a22c25
Javier Pena <javier@javierdev.com>
2016-09-16 10:50:58 -0500

    Deployment of stable angular2 app

--- app/soundcloud/keycodes.enum.js
+++ app/soundcloud/keycodes.enum.js
@@ -2,8 +2,9 @@
 (function (Keycodes) {
     Keycodes[Keycodes["Spacebar"] = 32] = "Spacebar";
     Keycodes[Keycodes["UpArrow"] = 38] = "UpArrow";
+    Keycodes[Keycodes["RightArrow"] = 39] = "RightArrow";
     Keycodes[Keycodes["DownArrow"] = 40] = "DownArrow";
 })(exports.Keycodes || (exports.Keycodes = {}));
 var Keycodes = exports.Keycodes;
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9zb3VuZGNsb3VkL2tleWNvZGVzLmVudW0udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLFdBQVksUUFBUTtJQUFHLGdEQUFhLENBQUE7SUFBRSw4Q0FBWSxDQUFBO0lBQUUsa0RBQWMsQ0FBQTtBQUFDLENBQUMsRUFBeEQsZ0JBQVEsS0FBUixnQkFBUSxRQUFnRDtBQUFwRSxJQUFZLFFBQVEsR0FBUixnQkFBd0QsQ0FBQSIsImZpbGUiOiJhcHAvc291bmRjbG91ZC9rZXljb2Rlcy5lbnVtLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGVudW0gS2V5Y29kZXMgeyBTcGFjZWJhciA9IDMyLCBVcEFycm93ID0gMzgsIERvd25BcnJvdyA9IDQwIH1cbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC9zb3VuZGNsb3VkL2tleWNvZGVzLmVudW0udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLFdBQVksUUFBUTtJQUFHLGdEQUFhLENBQUE7SUFBRSw4Q0FBWSxDQUFBO0lBQUUsb0RBQWUsQ0FBQTtJQUFFLGtEQUFjLENBQUE7QUFBQyxDQUFDLEVBQXpFLGdCQUFRLEtBQVIsZ0JBQVEsUUFBaUU7QUFBckYsSUFBWSxRQUFRLEdBQVIsZ0JBQXlFLENBQUEiLCJmaWxlIjoiYXBwL3NvdW5kY2xvdWQva2V5Y29kZXMuZW51bS5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBlbnVtIEtleWNvZGVzIHsgU3BhY2ViYXIgPSAzMiwgVXBBcnJvdyA9IDM4LCBSaWdodEFycm93ID0gMzksIERvd25BcnJvdyA9IDQwIH1cbiJdfQ==
