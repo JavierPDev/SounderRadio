32866d05a091c149c3e933e727b56e6c633f66c8
Javier Pena <javier@javierdev.com>
2016-09-20 11:32:52 -0500

    Throw error when no soundcloud client id

--- src/client/app/soundcloud/soundcloud.service.ts
+++ src/client/app/soundcloud/soundcloud.service.ts
@@ -7,6 +7,10 @@ export class SoundcloudService {
   private _clientId: string = String('<%= SOUNDCLOUD_CLIENT_ID %>');
 
   constructor(private _http: Http) {
+    if (!this._cliendId) {
+      throw new Error(`SoundCloud client id is required. Please attain one and 
+                      enter it in /tools/config/project.config.ts`);
+    }
   }
 
   public getTrack(trackId: number) {
